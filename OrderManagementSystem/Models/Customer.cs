e092e646de295f548aefde4dd95b1fff91e2f661
Derrick Abucheri <derrickwitness@gmail.com>
2025-06-24 22:28:43 +0300

    Set up database

---
+++ OrderManagementSystem/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using OrderManagementSystem.Enums;
+
+namespace OrderManagementSystem.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public CustomerSegment Segment { get; set; }
+	}
+}
---
+++ OrderManagementSystem/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using OrderManagementSystem.Enums;
+
+namespace OrderManagementSystem.Models
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public decimal TotalAmount { get; set; }
+		public OrderStatus Status { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime? DeliveredAt { get; set; }
+
+		public int CustomerId { get; set; }
+		public Customer Customer { get; set; }
+	}
+}
